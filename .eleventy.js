b37dc1f5b809f38e4600160bb70d8c03199eefc5
Tobias Andersen <turbolego@gmail.com>
2024-11-15 09:45:18 +0100

    bugfixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,26 @@
-module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("admin"); // Pass through CMS files
+const { DateTime } = require("luxon");
 
+module.exports = function (eleventyConfig) {
+    // Pass through CMS files
+    eleventyConfig.addPassthroughCopy("admin");
+
+    // Add "posts" collection
     eleventyConfig.addCollection("posts", function (collectionApi) {
         return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => {
             return new Date(b.date) - new Date(a.date); // Sort by date descending
         });
     });
 
+    // Add custom date filter
+    eleventyConfig.addFilter("date", (dateObj, format = "yyyy-MM-dd") => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(format);
+    });
+
     return {
         dir: {
             input: "src",
-            output: "_site"
-        }
+            output: "_site",
+        },
+        htmlTemplateEngine: "njk",
     };
 };
